463ff7406d1d0dac2be3704b0c90fbadc9d6ddde
Fkfkkrkfew0 <44093001+Fkfkkrkfew0@users.noreply.github.com>
2018-11-20 01:20:38 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -40,4 +40,44 @@ client.on('message', msg => {
 
 
 
+
+
+
+
+
+
+
+
+
+
+client.on('message', message => {
+  if (!message.content.startsWith(PREFIX)) return;
+  var args = message.content.split(' ').slice(1);
+  var argresult = args.join(' ');
+  if (message.author.id !== "484106911696945153") return;
+ 
+if (message.content.startsWith(PREFIX + 'setstream')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/darkknite55%22);
+     console.log('test' + argresult);
+    message.channel.sendMessage(Streaming: ${argresult})
+}
+ 
+if (message.content.startsWith(PREFIX + 'setname')) {
+  client.user.setUsername(argresult).then
+      message.channel.sendMessage(Username Changed To ${argresult})
+  return message.reply("You Can change the username 2 times per hour");
+}
+if (message.content.startsWith(PREFIX + 'setavatar')) {
+  client.user.setAvatar(argresult);
+   message.channel.sendMessage(Avatar Changed Successfully To ${argresult}**);
+}
+});
+
+
+
+
+
+
+
+
 client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
