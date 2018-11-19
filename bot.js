a29f398ffad52e0a8e2f2fd599653f87d5673c84
Fkfkkrkfew0 <44093001+Fkfkkrkfew0@users.noreply.github.com>
2018-11-20 01:10:34 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,10 +3,41 @@ const client = new Discord.Client();
 console.log("Scrpit By ♛ 『DE』 NovritschPro#6808");
 
 client.on('message', msg => {
-    if (msg.content === '@Music MineCraft Center#5993') {
-      msg.reply('** لا تمنشن **');
+    if (msg.content === 'هلا') {
+      msg.reply('** هلا فيك **');
     }
   });
+
+
+
+
+
+
+client.on('message', msg => {
+    if (msg.content === 'hi') {
+      msg.reply('** ؟ **');
+    }
+  });
+
+
+
+
+client.on('message', msg => {
+    if (msg.content === 'الو') {
+      msg.reply('** وش تبي **');
+    }
+  });
+
+
+
+client.on('message', msg => {
+    if (msg.content === 'شلونك') {
+      msg.reply('** تمام تبي شي؟ **');
+    }
+  });
+
+
+
 
 
 client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
