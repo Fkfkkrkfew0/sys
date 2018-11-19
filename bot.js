3e5a8e69ddbb8c8662d38134fe11bc3dca3270f9
Fkfkkrkfew0 <44093001+Fkfkkrkfew0@users.noreply.github.com>
2018-11-20 01:01:08 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,12 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By ♛ 『DE』 NovritschPro#6808");
+
 client.on('message', msg => {
     if (msg.content === '@NovritschPro_o#5808') {
       msg.reply('** لا تمنشن **');
     }
   });
+
+
+client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
