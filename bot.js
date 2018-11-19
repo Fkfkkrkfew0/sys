8f3c98bc7968e520f953f290448cf9503c9f7dc2
Fkfkkrkfew0 <44093001+Fkfkkrkfew0@users.noreply.github.com>
2018-11-20 01:30:06 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -50,28 +50,7 @@ client.on('message', msg => {
 
 
 
-client.on('message', message => {
-  if (!message.content.startsWith(PREFIX)) return;
-  var args = message.content.split(' ').slice(1);
-  var argresult = args.join(' ');
-  if (message.author.id !== "484106911696945153") return;
- 
-if (message.content.startsWith(PREFIX + 'setstream')) {
 
-     console.log('test' + argresult);
-    message.channel.sendMessage(Streaming: ${argresult})
-}
- 
-if (message.content.startsWith(PREFIX + 'setname')) {
-  client.user.setUsername(argresult).then
-      message.channel.sendMessage(Username Changed To ${argresult})
-  return message.reply("You Can change the username 2 times per hour");
-}
-if (message.content.startsWith(PREFIX + 'setavatar')) {
-  client.user.setAvatar(argresult);
-   message.channel.sendMessage(Avatar Changed Successfully To ${argresult}**);
-}
-});
 
 
 
